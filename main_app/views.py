a098a1bff49cc5799fcd9dd2ab9379a82a4c9757
hicass <cassidyjwalters@gmail.com>
2023-05-29 18:37:23 -0700

    Create functionality to see all Plants.

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+# Baby step:
+plants = [
+    {'name': 'Pothos', 'family': 'Araceae', 'care': 'Water every 1-2 weeks'},
+    {'name': 'Zee Zee Plant', 'family': 'Araceae', 'care': 'Water every 3-4 weeks'}
+]
+
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def plants_index(request):
+    return render(request, 'plants/index.html', {
+        'plants': plants
+    })
