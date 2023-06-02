df9d75c1a73368d7206ddd01510117391652e8af
hicass <cassidyjwalters@gmail.com>
2023-06-02 16:46:16 -0700

    Add the Photos Model to the admin portal.

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Plant, Watering, GrowingMedia
+from .models import Plant, Watering, GrowingMedia, Photo
 
 admin.site.register(Plant)
 admin.site.register(Watering)
 admin.site.register(GrowingMedia)
+admin.site.register(Photo)
 
